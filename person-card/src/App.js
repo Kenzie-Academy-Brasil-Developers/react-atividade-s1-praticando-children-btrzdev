2a69c3df4ace335b13de3022ccf679eb41684f16
Beatriz Silva <btrz.dev@gmail.com>
2021-10-28 15:08:03 -0300

    corrigido

--- person-card/src/App.js
+++ person-card/src/App.js
@@ -1,10 +1,23 @@
 import CenteredCard from "./components/CenteredCard";
+import Image from "./components/img/profile.png";
 
 function App() {
   return (
     <div className="App">
       <header className="container">
-        <CenteredCard />
+        <CenteredCard>
+          <div className="profile">
+            <p>
+              {" "}
+              <img src={Image} alt="Profile" />
+            </p>
+          </div>
+          <span>PROCURA-SE</span>
+          <div>
+            <p>HTML, CSS, JavaScript, React, React Native, NodeJS, MySQL</p>
+            <p>Full Stack</p>
+          </div>
+        </CenteredCard>
       </header>
     </div>
   );
